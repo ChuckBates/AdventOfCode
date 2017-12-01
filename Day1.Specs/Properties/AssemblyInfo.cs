206e0b57199bdb7fbad6bb9c65a90596ebdcc5b7
SovietBear <Numskull2@3#>
2017-12-01 00:55:33 -0700

    Inverse Captcha (Day1)(Part1)

---
+++ Day1.Specs/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Day1.Specs")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Day1.Specs")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("fb953bd8-16d2-48d3-a96b-64b871a00713")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Day1/InverseCaptcha.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+
+namespace Day1
+{
+    public class InverseCaptcha
+    {
+        public int GetSum(string puzzle)
+        {
+            var sum = 0;
+            if (!string.IsNullOrEmpty(puzzle) && puzzle.Length >= 2)
+            {
+                var digits = puzzle.ToCharArray();
+                for (int i = 0; i < digits.Length; i++)
+                {
+                    int next = i + 1 < digits.Length ? digits[i + 1] : digits[0];
+                    var current = digits[i];
+                    if (current == next)
+                    {
+                        sum += (int) char.GetNumericValue(current);
+                    }
+                }
+            }
+            return sum;
+        }
+    }
+}
