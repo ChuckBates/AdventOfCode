c0acff0aa26fb0cfcbe635d46775c41f762967e4
SovietBear <Numskull2@3#>
2017-12-01 01:10:10 -0700

    Inverse Captcha (Day1)(Part2)

--- Day1/InverseCaptcha.cs
+++ Day1/InverseCaptcha.cs
@@ -22,5 +22,40 @@ namespace Day1
             }
             return sum;
         }
+
+        public int GetHalfwaySum(string puzzle)
+        {
+            var sum = 0;
+            var half = puzzle.Length / 2;
+            if (!string.IsNullOrEmpty(puzzle) && puzzle.Length >= 2)
+            {
+                var digits = puzzle.ToCharArray();
+                for (int i = 0; i < digits.Length; i++)
+                {
+                    int next = GetNextHalfway(i, half, digits);
+                    var current = digits[i];
+                    if (current == next)
+                    {
+                        sum += (int)char.GetNumericValue(current);
+                    }
+                }
+            }
+            return sum;
+        }
+
+        private char GetNextHalfway(int index, int half, char[] digits)
+        {
+            char next;
+            if (index + half >= digits.Length)
+            {
+                next = digits[index + half - digits.Length];
+            }
+            else
+            {
+                next = digits[index + half];
+            }
+
+            return next;
+        }
     }
 }
