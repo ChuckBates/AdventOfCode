c9d478bfee2a093a8263ebae350128ade82a0e95
SovietBear <Numskull2@3#>
2017-12-04 23:48:27 -0700

    Jump Maze (Day 5)(Part 1)

---
+++ Day5.Specs/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Day5.Specs")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Day5.Specs")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("8ad8c5d1-610c-4d9c-9b2c-3e9bb6f7e559")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Day5/JumpMaze.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+
+namespace Day5
+{
+    public class JumpMaze
+    {
+        public string[] Offsets;
+
+        public int GetStepsToEscape(string maze)
+        {
+            if (string.IsNullOrEmpty(maze))
+            {
+                return 0;
+            }
+
+            Offsets = maze.Split(' ');
+            if (int.Parse(Offsets[0]) >= maze.Length)
+            {
+                return 1;
+            }
+
+            var steps = 0;
+            var position = 0;
+            while (true)
+            {
+                steps++;
+                var currentOffset = Offsets[position];
+                if (int.Parse(currentOffset) >= Offsets.Length - position)
+                {
+                    return steps;
+                }
+                Offsets[position] = (int.Parse(currentOffset) + 1).ToString();
+                position = position + int.Parse(currentOffset);
+            }
+        }
+    }
+}
