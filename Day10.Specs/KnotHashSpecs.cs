0f692d7b673b85493cbbf8935fedc66726fc6620
Chuck Bates <chuck-bates@pluralsight.com>
2017-12-10 00:06:50 -0700

    Knot Hash (Day 10)(Part 1)

---
+++ Day10.Specs/KnotHashSpecs.cs
@@ -0,0 +1,72 @@
+﻿using NUnit.Framework;
+
+namespace Day10.Specs
+{
+    [TestFixture]
+    public class KnotHashSpecs
+    {
+        KnotHash ClassUnderTest;
+
+        [TestFixtureSetUp]
+        public void Setup()
+        {
+            ClassUnderTest = new KnotHash();
+            ClassUnderTest.KeyList = ClassUnderTest.GenerateList();
+        }
+
+        [Test]
+        public void when_hashing_a_knot_and_the_lengths_are_null_empty()
+        {
+            var result = ClassUnderTest.Hash(null);
+            Assert.AreEqual(0, result);
+
+            result = ClassUnderTest.Hash(new int[] { });
+            Assert.AreEqual(0, result);
+        }
+
+        [Test]
+        public void when_hashing_a_knot_and_there_is_one_length_of_one()
+        {
+            var lengths = new[] { 1 };
+            var result = ClassUnderTest.Hash(lengths);
+            Assert.AreEqual(0, result);
+        }
+
+        [Test]
+        public void when_hashing_a_knot_and_there_is_one_length_greater_than_256()
+        {
+            var lengths = new[] { 1000 };
+            var result = ClassUnderTest.Hash(lengths);
+            Assert.AreEqual(0, result);
+        }
+
+        [Test]
+        public void when_hashing_a_knot_with_test_marks_example()
+        {
+            ClassUnderTest.KeyList = GenerateList();
+
+            var lengths = new[] { 3, 4, 1, 5 };
+            var result = ClassUnderTest.Hash(lengths);
+            Assert.AreEqual(12, result);
+        }
+
+        [Test]
+        public void when_hashing_a_knot_with_puzzle_input()
+        {
+            var lengths = new[] { 157, 222, 1, 2, 177, 254, 0, 228, 159, 140, 249, 187, 255, 51, 76, 30 };
+            var result = ClassUnderTest.Hash(lengths);
+            Assert.AreEqual(62238, result);
+        }
+
+        private int[] GenerateList()
+        {
+            var list = new int[5];
+            for (int i = 0; i < 5; i++)
+            {
+                list[i] = i;
+            }
+
+            return list;
+        }
+    }
+}
---
+++ Day10/KnotHash.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day10
+{
+    public class KnotHash
+    {
+        public int[] KeyList { get; set; }
+
+        public int Hash(int[] lengths)
+        {
+            if (lengths == null || lengths.Length == 0)
+            {
+                return 0;
+            }
+
+            var currentPosition = 0;
+            var skipSize = 0;
+            var list = KeyList;
+            foreach (var length in lengths)
+            {
+                if (length > 256)
+                {
+                    continue;
+                }
+
+                list = SelectAndTwist(list, length, currentPosition);
+
+                currentPosition += length + skipSize;
+                if (currentPosition >= list.Length)
+                {
+                    currentPosition = currentPosition - list.Length;
+                }
+                skipSize++;
+            }
+
+            return list[0] * list[1];
+        }
+
+        private int[] SelectAndTwist(int[] list, int length, int currentPosition)
+        {
+            var selection = new List<int>();
+            var index = currentPosition;
+            for (int i = 0; i < length; i++)
+            {
+                if (index >= list.Length)
+                {
+                    index = 0;
+                }
+                selection.Add(list[index]);
+                index++;
+            }
+
+            selection.Reverse();
+
+            index = currentPosition;
+            foreach (var mark in selection)
+            {
+                if (index >= list.Length)
+                {
+                    index = 0;
+                }
+                list[index] = mark;
+                index++;
+            }
+
+            return list;
+        }
+
+        public int[] GenerateList()
+        {
+            var list = new int[256];
+            for (int i = 0; i < 256; i++)
+            {
+                list[i] = i;
+            }
+
+            return list;
+        }
+    }
+}
