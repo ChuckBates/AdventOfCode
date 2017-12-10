62c52b91d5064ec32ca0e474dca914eff2fdad8e
Chuck Bates <chuck-bates@pluralsight.com>
2017-12-09 21:28:52 -0700

    Stream Processing (Day 9)(Part 1)

---
+++ Day9.Specs/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Day9.Specs")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Day9.Specs")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("e232c2ad-4d3a-4c7e-be1c-29c514d9ad2f")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Day9/StreamProcessor.cs
@@ -0,0 +1,37 @@
+﻿namespace Day9
+{
+    public class StreamProcessor
+    {
+        public int Score(string stream)
+        {
+            var level = 1;
+            var score = 0;
+            var mode = '{';
+            for (var i = 0; i < stream.ToCharArray().Length; i++)
+            {
+                var character = stream.ToCharArray()[i];
+                switch (character)
+                {
+                    case '{' when !mode.Equals('<'):
+                        score += level;
+                        level++;
+                        break;
+                    case '}' when !mode.Equals('<'):
+                        level--;
+                        break;
+                    case '<':
+                        mode = '<';
+                        break;
+                    case '>':
+                        mode = '{';
+                        break;
+                    case '!' when mode.Equals('<'):
+                        i++;
+                        break;
+                }
+            }
+
+            return score;
+        }
+    }
+}
