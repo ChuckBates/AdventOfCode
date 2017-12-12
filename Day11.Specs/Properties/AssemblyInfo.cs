10fe83ec16de18eb91bbab9599c7e6b4884b34e4
Chuck Bates <chuck-bates@pluralsight.com>
2017-12-11 23:19:49 -0700

    Hex Ed (Day 11)(Part 1)

---
+++ Day11.Specs/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Day11.Specs")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Day11.Specs")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("56947c64-ea7c-4969-bac6-70f132a4cfa8")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Day11/HexEd.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Day11
+{
+    public class HexEd
+    {
+        public int FewestSteps(string childPath)
+        {
+            if (string.IsNullOrEmpty(childPath))
+            {
+                return 0;
+            }
+
+            var coordinates = new[] {0, 0, 0};
+            foreach (var step in childPath.Split(','))
+            {
+                coordinates = Move(coordinates, step);
+            }
+
+            return (Math.Abs(coordinates[0] - 0) + Math.Abs(coordinates[1] - 0) + Math.Abs(coordinates[2] - 0)) / 2;
+        }
+
+        public int[] Move(int[] currentCoordinates, string direction)
+        {
+            switch (direction)
+            {
+                case "n":
+                    return new[] {currentCoordinates[0], currentCoordinates[1] + 1, currentCoordinates[2] - 1};
+                case "ne":
+                    return new[] {currentCoordinates[0] + 1, currentCoordinates[1], currentCoordinates[2] - 1};
+                case "se":
+                    return new[] {currentCoordinates[0] + 1, currentCoordinates[1] - 1, currentCoordinates[2]};
+                case "s":
+                    return new[] {currentCoordinates[0], currentCoordinates[1] - 1, currentCoordinates[2] + 1};
+                case "sw":
+                    return new[] {currentCoordinates[0] - 1, currentCoordinates[1], currentCoordinates[2] + 1};
+                case "nw":
+                    return new[] {currentCoordinates[0] - 1, currentCoordinates[1] + 1, currentCoordinates[2]};
+                default:
+                    return currentCoordinates;
+            }
+        }
+    }
+}
