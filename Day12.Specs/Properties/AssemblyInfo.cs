411a752eed9b03127fe749675c2f7b4a8b578d4a
Chuck Bates <chuck-bates@pluralsight.com>
2017-12-12 00:14:41 -0700

    Digital Plumber (Day 12)(Part 1)

---
+++ Day12.Specs/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Day12.Specs")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Day12.Specs")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("1c721165-46b8-4473-9fd5-8442cfde22f0")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Day12/DigitalPlumber.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Day12
+{
+    public class DigitalPlumber
+    {
+        Dictionary<int, int[]> pipes = new Dictionary<int, int[]>();
+
+        public Dictionary<int, int[]> ParsePipes(string pipesPrimitve)
+        {
+            var pipes = new Dictionary<int, int[]>();
+            if (!string.IsNullOrEmpty(pipesPrimitve))
+            {
+                foreach (var pipe in pipesPrimitve.Split('\n'))
+                {
+                    var pipePieces = pipe.Split(' ');
+                    pipes.Add(int.Parse(pipePieces[0]), ParsePieces(pipePieces[2]));
+                }
+            }
+            return pipes;
+        }
+
+        public int[] ParsePieces(string connectedPipes)
+        {
+            var pipes = connectedPipes.Split(',');
+            var pieces = new int[pipes.Length];
+            for (var i = 0; i < pipes.Length; i++)
+            {
+                pieces[i] = int.Parse(pipes[i].Trim());
+            }
+
+            return pieces;
+        }
+
+        public int PipesConnectedToProgramZero(string pipeList)
+        {
+            pipes = ParsePipes(pipeList);
+            var connectedPrograms = new List<int>();
+            connectedPrograms.Add(0);
+            AddConnectedPrograms(connectedPrograms, pipes[0]);
+
+            return connectedPrograms.Count;
+        }
+
+        public void AddConnectedPrograms(List<int> connectedPrograms, int[] programs)
+        {
+            foreach (var program in programs)
+            {
+                if (!connectedPrograms.Contains(program))
+                {
+                    connectedPrograms.Add(program);
+                    AddConnectedPrograms(connectedPrograms, pipes[program]);
+                }
+            }
+        }
+    }
+}
